296ca013f7a764002ac45d033d064dd7fb74fd49
sMeatBear <fcandroidme@gmail.com>
2019-12-23 12:05:01 -0800

    create down casting test

--- javarevise/src/app/basic/DownCasting.java
+++ javarevise/src/app/basic/DownCasting.java
@@ -1,5 +1,20 @@
+package basic;
+
+class Father {
+    String name = "father";
+    public void test() {
+        System.out.println("hello");
+    }
+}
+
+class Child extends Father{
+    String name = "child";
+}
+
 public class DownCasting {
     public static void main(String[] args) {
-        System.out.println("hello");
+        Father f = new Father();
+        Father c = new Child();
+        System.out.println(f instanceof Child);
     }
 }
