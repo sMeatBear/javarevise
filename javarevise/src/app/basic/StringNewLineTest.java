53e434ee19fd57904c4feebb13b8505533a80fca
sMeatBear <fcandroidme@gmail.com>
2021-11-09 18:30:39 +0800

    Create java new line of stream

---
+++ javarevise/src/app/basic/StringNewLineTest.java
@@ -0,0 +1,9 @@
+package basic;
+
+public class StringNewLineTest {
+    public static void main(String[] args) {
+        String multiLines = "test sa  asdflkj" + 
+        "sadf asdfl asdfj saldfj asdf sad asdflkj sadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflk";
+        System.out.println(multiLines);
+    }
+}
