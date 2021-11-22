17c599695dac8d629b1f3f01ced64b67a581e2b9
sMeatBear <fcandroidme@gmail.com>
2021-11-23 00:46:09 +0800

    Create StringNewLineTest

--- javarevise/src/app/basic/StringNewLineTest.java
+++ javarevise/src/app/basic/StringNewLineTest.java
@@ -5,5 +5,6 @@ public class StringNewLineTest {
         String multiLines = "test sa  asdflkj" + 
         "sadf asdfl asdfj saldfj asdf sad asdflkj sadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflksadf asdfl asdfj saldfj asdf sad asdflk";
         System.out.println(multiLines);
+        System.out.println((new String[] {"1"}).getClass().getName());
     }
 }
